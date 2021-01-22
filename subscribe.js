05893fdf21bec05f72db3359dfe45a1e2acd3ace
Saad Saif <saadgfx97@gmail.com>
2021-01-23 01:28:05 +0500

    subscribe example

---
+++ subscribe.js
@@ -0,0 +1,34 @@
+const { createStore } = require('redux')
+
+const increment = () => {
+  return { type: 'increment' }
+}
+
+const decrement = () => {
+  return { type: 'decrement' }
+}
+
+const initialState = 0
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const store = createStore(countReducer)
+
+// Define your change listener function here.
+function printCountStatus() {
+  console.log(`The count is ${store.getState()}`)
+}
+
+const unsubscribe = store.subscribe(printCountStatus)
+
+store.dispatch(decrement()) // store.getState() === -1
+store.dispatch(increment()) // store.getState() === 0
+store.dispatch(increment()) // store.getState() === 1
